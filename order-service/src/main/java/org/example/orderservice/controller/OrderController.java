fccb4ff5f6f200a52d397dcd1767a41b2c60cef1
Kavindu <gayanthakavindu3@gmail.com>
2025-03-03 09:00:19 +0530

    created order controller

---
+++ order-service/src/main/java/org/example/orderservice/controller/OrderController.java
@@ -0,0 +1,14 @@
+package org.example.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @GetMapping
+    public String getOrder() {
+        return "Order service : order-001";
+    }
+}
